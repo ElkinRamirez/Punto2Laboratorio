3559d793b1e3a50c6bf5d7bc0c777605267284f3
ELKIN RAMIREZ <ELKIN RAMIREZ@ELKIN_RAMIREZPC>
2018-09-21 16:28:25 -0500

    21 de septiembre

---
+++ src/LibreriaRuiz.java
@@ -0,0 +1,21 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ELKIN RAMIREZ
+ */
+public class LibreriaRuiz {
+    private Libro [] libros;
+    /**
+     *
+     * @param args
+     */
+
+    public static void main(String[] args) {
+    }
+    
+}
